13e5b81e2ee891af3f801e03a07163ad042bda45
czm23l <lauravscz@gmail.com>
2025-06-04 12:31:21 +0300

    chore: update VS Code settings and main script

--- .vscode/python main.py
+++ .vscode/python main.py
@@ -1,4 +1,5 @@
 import requests
+from bitcoinlib.wallets import Wallet
 
 def get_balance(address):
     url = f"https://blockchain.info/q/addressbalance/{address}"
@@ -6,6 +7,11 @@ def get_balance(address):
     if r.ok:
         return int(r.text) / 1e8  # BTC
     return 0
+
+def send_btc(private_key_wif, to_address, amount_btc):
+    w = Wallet.create('tempwallet', keys=private_key_wif, network='bitcoin')
+    tx = w.send_to(to_address, amount_btc, network='bitcoin')
+    print("Tranzacție trimisă:", tx.txid)
 
 # Your Python code goes here, for example:
 def main():
