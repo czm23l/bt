6b5b1dc05d2ef2c3909c1065e66b26bba73da296
czm23l <lauravscz@gmail.com>
2025-06-04 12:31:03 +0300

    chore: update VS Code settings and main script

--- .vscode/python main.py
+++ .vscode/python main.py
@@ -1,3 +1,12 @@
+import requests
+
+def get_balance(address):
+    url = f"https://blockchain.info/q/addressbalance/{address}"
+    r = requests.get(url)
+    if r.ok:
+        return int(r.text) / 1e8  # BTC
+    return 0
+
 # Your Python code goes here, for example:
 def main():
 	print("Hello, world!")
